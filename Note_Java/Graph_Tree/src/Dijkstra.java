6d27ae372e6a2f93665ec6e6f31dc614c75be875
Oflo <57768558+mmkser10@users.noreply.github.com>
2021-03-17 14:29:27 +0900

    feat : 다익스트라 알고리즘 구현(배열, 우선순위큐)

---
+++ Note_Java/Graph_Tree/src/Dijkstra.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class Dijkstra {
+    private int n;
+    private int[][] graph;
+
+    public Dijkstra(int n){
+        this.n = n;
+        this.graph = new int[n][n];
+    }
+
+    public void put(int start, int end, int weight){
+        graph[start-1][end-1] = weight;
+    }
+
+    public void searchDistance(int start){
+        int[] distance = new int[n];
+        boolean[] check = new boolean[n];
+
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start-1] = 0;
+
+        for(int x = 0 ; x < n-1 ; x++){
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+
+            for(int y = 0 ; y < n ; y++){
+                if(!check[y] && min > distance[y]){
+                    index = y;
+                    min = distance[y];
+                }
+            }
+
+            for(int y = 0 ; y < n ; y++){
+                if(
+                        !check[y]
+                        && graph[index][y] != 0
+                        && distance[index] != Integer.MAX_VALUE
+                        && distance[index] + graph[index][y] < distance[y])
+                {
+                    distance[y] = distance[index] + graph[index][y];
+                }
+            }
+            check[index] = true;
+        }
+        System.out.println(Arrays.toString(distance));
+    }
+
+    public static void main(String[] args) {
+        Dijkstra dijkstra = new Dijkstra(8);
+        dijkstra.put(1, 2, 3);
+        dijkstra.put(1, 5, 4);
+        dijkstra.put(1, 4, 4);
+        dijkstra.put(2, 3, 2);
+        dijkstra.put(3, 4, 1);
+        dijkstra.put(4, 5, 2);
+        dijkstra.put(5, 6, 4);
+        dijkstra.put(4, 7, 6);
+        dijkstra.put(7, 6, 3);
+        dijkstra.put(3, 8, 3);
+        dijkstra.put(6, 8, 2);
+        dijkstra.searchDistance(1);
+    }
+}
---
+++ Note_Java/Graph_Tree/src/Dijkstra_PQ.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Dijkstra_PQ{
+    private int n;
+    private List<Edge>[] graph;
+    private class Edge implements Comparable<Edge> {
+        int v, weight;
+
+        public Edge(int v, int weight) {
+            this.v = v;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            // TODO Auto-generated method stub
+            return Integer.compare(this.weight, o.weight);
+        }
+
+        @Override
+        public String toString() {
+            return weight + "";
+        }
+
+    }
+
+    public Dijkstra_PQ(int n){
+        this.n = n;
+        graph = new ArrayList[n];
+        for(int x = 0 ; x < n ; x++) {
+            graph[x] = new ArrayList<>();
+        }
+    }
+
+    public void put(int start, int end, int weight){
+        for(int x = 0 ; x < n ; x++){
+            graph[start-1].add(new Edge(end-1, weight));
+        }
+    }
+
+    public void searchDistance(int start){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        boolean[] check = new boolean[n];
+        Edge[] distance = new Edge[n];
+
+        for(int x = 0 ; x < n ; x++){
+            if (x == start-1) {
+                distance[x] = new Edge(x, 0);
+            }
+            else {
+                distance[x] = new Edge(x, Integer.MAX_VALUE);
+            }
+            pq.add(distance[x]);
+        }
+        while(!pq.isEmpty()){
+            Edge edge = pq.poll();
+
+            for(Edge next : graph[edge.v]){
+                if(!check[next.v] && distance[next.v].weight > distance[edge.v].weight + next.weight) {
+                    distance[next.v].weight = distance[edge.v].weight+next.weight;
+                    pq.remove(distance[next.v]);
+                    pq.add(distance[next.v]);
+                }
+            }
+            check[edge.v] = true;
+        }
+        System.out.println(Arrays.toString(distance));
+    }
+
+    public static void main(String[] args) {
+        Dijkstra_PQ dijkstra_pq = new Dijkstra_PQ(8);
+        dijkstra_pq.put(1, 2, 3);
+        dijkstra_pq.put(1, 5, 4);
+        dijkstra_pq.put(1, 4, 4);
+        dijkstra_pq.put(2, 3, 2);
+        dijkstra_pq.put(3, 4, 1);
+        dijkstra_pq.put(4, 5, 2);
+        dijkstra_pq.put(5, 6, 4);
+        dijkstra_pq.put(4, 7, 6);
+        dijkstra_pq.put(7, 6, 3);
+        dijkstra_pq.put(3, 8, 3);
+        dijkstra_pq.put(6, 8, 2);
+        dijkstra_pq.searchDistance(1);
+    }
+}
